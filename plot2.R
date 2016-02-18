ed3e06c3da76ce02855d048f07a2ad24345004b2
Suresh Krishnan <krisurez@gmail.com>
2016-02-17 19:55:00 -0500

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,15 @@
+plot2 <- function()
+{
+  #read the text file
+  plot2fulldata <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+  
+  #subset the data based on date
+  plot2data <- plot2fulldata[plot2fulldata$Date %in% c("1/2/2007","2/2/2007") ,]
+  
+  datetime <- strptime(paste(plot2data$Date, plot2data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+  
+  GlobalActivePower <- as.numeric(plot2data$Global_active_power)
+  
+  plot(datetime,GlobalActivePower,type="l",xlab="",ylab = "Global Active Power (kilowatts)")
+  
+}
