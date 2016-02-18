a5edaf940a9cfe8fb3df9c33f193a012b4bcc855
Suresh Krishnan <krisurez@gmail.com>
2016-02-17 19:54:14 -0500

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,11 @@
+plot1 <- function()
+{
+  #read the text file
+  plot1fulldata <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+  
+  #subset the data based on date
+  plot1data <- plot1fulldata[plot1fulldata$Date %in% c("1/2/2007","2/2/2007") ,]
+  
+  hist(as.numeric(plot1data$Global_active_power),col="red",main = "Global Active Power" , xlab = "Global Active Power (kilowatts)")
+  
+}
